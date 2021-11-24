2b0de4d2da75b11ea068627f3ebbd5f536e57676
Kaio Postal <kaiopostal@gmail.com>
2021-11-24 12:06:59 -0300

    Sistema de pizzaria finalizado

--- script.js
+++ script.js
@@ -117,18 +117,46 @@ c('.pizzaInfo--addButton').addEventListener('click', () => {
         });
     }
 
-    
+
     closeModal();
     updateCart();
     //Quantas pizzas? modalQt
 });
 
+//Abre carrinho mobile
+
+c('.menu-openner').addEventListener('click', () => {
+    if (cart.length > 0) {
+        c('aside').style.left = '0';
+    }
+
+});
+
+//Fecha menu carrinho mobile
+
+
+c('.menu-closer').addEventListener('click', ()=>{
+     c('aside').style.left = '100vw'
+})
+
 function updateCart() {
+
+    c('.menu-openner span').innerHTML = cart.length;
+
+
+
     if (cart.length > 0) {
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
+
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
+
         for (let i in cart) {
             let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id); //Resgastar informações da pizza via ID
+            subtotal += pizzaItem.price * cart[i].qt;
             let cartItem = c('.models .cart--item').cloneNode(true);
             //Adiciona nome e tamanho da pizza no Cart
             let pizzaSizeName;
@@ -146,16 +174,37 @@ function updateCart() {
             let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`; //Junta o nome da pizza com o tamanho
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
-            cartItem.querySelector('cart--item--qt').innerHTML = cartItem[i].qt;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if (cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1); //Remove o item do carrinho 
+                }
+
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qt++;
+                updateCart();
+            });
 
 
             c('.cart').append(cartItem);
-
-
-
         }
+
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
+
     } else {
         c('aside').classList.remove('show');
+
+        //Fecha o carrinho mobile quando a quantidade de itens esta menor que 1
+        c('aside').style.left = '100vw';
 
     }
 
